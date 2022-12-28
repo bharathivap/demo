c3c0d430f6cb390ddc8451ad2039381fb6d4ade1
bharathivap <bharathi1996don@gmail.com>
2022-12-28 14:50:06 +0530

    checking first time commit

--- src/main/java/task/create_api/demo/controller/testing/Testing.java
+++ src/main/java/task/create_api/demo/controller/testing/Testing.java
@@ -3,7 +3,13 @@ package task.create_api.demo.controller.testing;
 public class Testing {
     int a = 5;
     int b = 10;
-    int c = a+b;
+    int c = a + b;
+
+    public  void print{
+        String b = "My Self Testing Commit";
+        System.out.print(b);
+    }
 
 }
+
 
